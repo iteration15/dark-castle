833d4600bdb70faedcee41147127930422e3fa2f
dean <dean@iteration15.com>
2015-08-02 00:27:37 +0000

    added hero graphics

--- main/classes/constants.py
+++ main/classes/constants.py
@@ -19,12 +19,21 @@ MAXHEALTH = 3
 
 IMG_DIR = join(dirname(dirname(abspath(__file__))),"graphics") + sep
 
-IMG_WALL = 'wall.png'
-IMG_FLOOR = 'floor.png'
 IMG_BG_LVL1 = 'bglvl1.png'
 IMG_HERO_D = 'hero_down.png'
+IMG_HERO_D3 = 'hero_down3.png'
+IMG_HERO_L1 = 'hero_left1.png'
+IMG_HERO_L2 = 'hero_left2.png'
+IMG_HERO_L3 = 'hero_left3.png'
+IMG_HERO_R1 = 'hero_right1.png'
+IMG_HERO_R2 = 'hero_right2.png'
+IMG_HERO_R3 = 'hero_right3.png'
+IMG_HERO_U1 = 'hero_up1.png'
+IMG_HERO_U2 = 'hero_up2.png'
+IMG_HERO_U3 = 'hero_up3.png'
 
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
+openop
